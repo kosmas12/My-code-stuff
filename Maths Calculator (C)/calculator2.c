0ac9897f9534a3a7e5a96bc492b11b39a6a1ca8b
Kosmas Raptis <keeperkosmas6@gmail.com>
2020-04-03 18:20:24 +0300

    Fix attempts for the new calculator

--- Maths Calculator (C)/calculator2.c
+++ Maths Calculator (C)/calculator2.c
@@ -3,27 +3,11 @@
 #include <math.h>
 #define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
 
-static float addition(float* n, int c) { return n[0] + n[1]; }
-
-static float multiplication(float* n, int c) { return n[0] * n[1]; }
-
-static float division(float *n, int c) {return n[0] / n[1];}
-
-static float subtraction(float *n, int c) {return n[0] - n[1];}
-
-static float help(float *n, int c) 
-{
-    for(int i = 0; i < ARRAY_SIZE(operations); i++) 
-    {
-        printf("%s: %s", o->command, o->description);
-    }
-}
-
 typedef struct {
-        const char command;
+        char command;
         const char* description;
         int inputs;
-        float(*handler)(float* n, int c);
+        float(*handler)(float a, float b);
     }MathOperation;
 
 static MathOperation operations[] = {
@@ -31,19 +15,46 @@ static MathOperation operations[] = {
   { .command='*', .description="Multiplication", .inputs=2, .handler=multiplication }
   { .command='/', .description="Division", .inputs=2, .handler=division },
   { .command='-', .description="Subtraction", .inputs=2, .handler=subtraction },
-  { .command='h', .description= "This help page", .inputs=0, .handler=help}
+  { .command='h', .description= "This help page", .inputs=0, .handler=help},
+  { .command='q', .description="Quit", .inputs=0, .handler=help}
 };
+
+
+static float addition(float a, float b) { return a + b; }
+
+static float multiplication(float a, float b) { return a * b; }
+
+static float division(float a, float b) {return a / b;}
+
+static float subtraction(float a, float b) {return a - b;}
+
+static float help(float a, float b) 
+{
+    for(int i = 0; i < ARRAY_SIZE(operations); i++) 
+    {
+        printf("%c: %s\n", o->command, o->description);
+    }
+}
+
 
 int main()
 {
-    MathOperation* o = &operations[i];
-    scanf(" %c", &command);
-    for(int i = 0; i < ARRAY_SIZE(operations); i++) 
-    {   
-        if (o->command == command)
+    while (command != 'q')
+    {
+        float user_input = 0.0f;
+        float result = 0.0f;
+
+        MathOperation* o = &operations[i];
+        scanf(" %c", &command);
+        for(int i = 0; i < ARRAY_SIZE(operations); i++) 
         {
-            handler = o->handler;
+   
+            if (o->command == command)
+            {
+                result = o->handler(result, user_input);
+            }
         }
+
     }
     
 }
