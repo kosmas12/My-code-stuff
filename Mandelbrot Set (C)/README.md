39f69a9ce8f6120c0c60e41f91314304f0a828a9
Kosmas Raptis <keeperkosmas6@gmail.com>
2020-04-22 21:51:48 +0300

    Started work on NXDK port for Mandelbrot set

---
+++ Mandelbrot Set (C)/README.md
@@ -0,0 +1,31 @@
+# mandelbrot-c-sdl2
+
+Using the C language and the SDL2 library to draw the Mandelbrot set.
+
+## Features
+
+* Use the **arrow keys** to move
+* Use the **+ key** to zoom in
+* Use the **- key** to zoom out
+
+## Requirements
+
+* C standard library with (G)CC
+* SDL2 library
+* Possibly MinGW if running Windows
+
+## Compilation
+
+```
+$ make help
+Just use make (without any argument) to compile.
+Use make clean in order to delete mandelbrot.
+If you have errors during compilation, you can contact me for help.
+Windows users, you can use MinGW with this project.
+```
+
+Then run the program and enjoy ! :-)
+
+![screenshot](screenshot.png "This is beautiful...")
+
+Happy fractal !
---
+++ Mandelbrot Set (C)/mandelbrot.c
@@ -0,0 +1,38 @@
+#if defined (NXDK)
+#include <SDL.h>
+#include <SDL_gamecontroller.h>
+#include <hal/debug.h>
+#include <hal/video.h>
+#include <windows.h>
+#define printf(...) debugPrint(__VA_ARGS__)
+#else
+#include <SDL2/SDL.h>
+#endif
+#include "headers/mySDL.h"
+#include "headers/myStructures.h"
+#include "headers/myFractal.h"
+
+// Any other main prototype will bug MinGW's SDL2
+// Doesn't matter on *nix
+int main(int argc, char **argv) {
+  XVideoSetMode(640, 480, 32, REFRESH_60HZ);
+  // Init the structures
+  Sdl *sdl = init_sdl();
+  Fractal *fractal = init_fractal();
+
+  // Init console
+  print_verbose(fractal);
+
+  // User can exit program using escape
+  while (is_user_pressing_escape(sdl) == 0) {
+    draw_mandelbrot(sdl, fractal);
+    is_user_moving(sdl, fractal);
+
+    SDL_RenderPresent(sdl->renderer);
+
+    SDL_Delay(10);
+  }
+
+  free_everything(sdl, fractal);
+  return 0;
+}
