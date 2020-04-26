8335ac5ce95ae9ee28be27ef016900be9ffa2947
Kosmas Raptis <keeperkosmas6@gmail.com>
2020-04-26 22:20:10 +0300

    Added Fibonacci in C

---
+++ Fibonacci Sequnce (C)/main.c
@@ -0,0 +1,18 @@
+int main()
+{
+    int n1 = 0;
+    int n2 = 1;
+
+    int nn = n1 + n2;
+    for (int i = 0; i < 100; i++)
+    {
+        printf("%d\n", nn);
+        n1 = nn;
+        nn = n1 + n2;
+        printf("%d\n", nn);
+        n2 = nn;
+        nn = n1 + n2;
+        printf("%d\n", nn);
+    }
+    
+}
