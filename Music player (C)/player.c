0f128a81ff7eb2acda75dac3b2df3635f8f4ff12
Kosmas Raptis <keeperkosmas6@gmail.com>
2020-05-14 10:55:22 +0300

    The player can now play an audio file that doesn't exceed Xbox RAM limitations

--- Music player (C)/player.c
+++ Music player (C)/player.c
@@ -1,6 +1,8 @@
 #if defined (NXDK)
 #include <hal/video.h>
 #include <windows.h>
+#include <hal/xbox.h>
+#include <hal/debug.h>
 #include <SDL.h>
 #include <SDL_audio.h>
 #else
@@ -11,19 +13,19 @@
 
 int main()
 {
-
     #if defined (NXDK)
     XVideoSetMode(640, 480, 32, REFRESH_DEFAULT);
+    debugClearScreen();
     #endif
 
-    SDL_Init(SDL_INIT_AUDIO|SDL_INIT_VIDEO);
+    SDL_Init(SDL_INIT_AUDIO);
 
     // declare the needed variables
     SDL_AudioSpec wavSpec;
     Uint32 wavLength;
     Uint8 *wavBuffer;
 
-    SDL_LoadWAV("songname", &wavSpec, &wavBuffer, &wavLength); // FIXME: Support user selected file
+    SDL_LoadWAV("D:\\nxdk.wav", &wavSpec, &wavBuffer, &wavLength); // FIXME: Support user selected file
 
     SDL_AudioDeviceID deviceID = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0); //NULL means default
 
@@ -31,7 +33,7 @@ int main()
 
     SDL_PauseAudioDevice(deviceID, 0); //SDL_PauseAudioDevice with 0 in place of pause_on means unpaused, thus playing
 
-    SDL_Delay(246000); //FIXME: Make it so that we determine the length of a song and Delay with that length in ms
+    SDL_Delay(3000); //FIXME: Make it so that we determine the length of a song and Delay with that length in ms
 
     SDL_CloseAudioDevice(deviceID);
 	SDL_FreeWAV(wavBuffer);
