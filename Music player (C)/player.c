d52a9d4ab5003a2c6f34e934553635b9b8e87d3c
Kosmas Raptis <keeperkosmas6@gmail.com>
2020-05-16 00:35:14 +0300

    The program now runs for as long as there is audio, and streams data as needed

--- Music player (C)/player.c
+++ Music player (C)/player.c
@@ -13,6 +13,21 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+static Uint8 *audio_position; 
+static Uint32 audio_length;
+
+void audio_callback(void *userdata, Uint8 *stream, int len) {
+	
+	if (audio_length ==0)
+		return;
+	
+	len = ( len > audio_length ? audio_length : len );
+	SDL_memcpy (stream, audio_position, len); 					// simply copy from one buffer into the other
+	
+	audio_position += len;
+	audio_length -= len;
+}
+
 void Quit(SDL_AudioDeviceID deviceID, Uint8 *wavBuffer) {
     SDL_CloseAudioDevice(deviceID);
 	SDL_FreeWAV(wavBuffer);
@@ -40,78 +55,83 @@ static bool isNewlyPressed(bool is_held, bool *was_held) {
 int main()
 {
 
-    bool a_is_held = true;
-    bool a_was_held = true;
+  bool a_is_held = true;
+  bool a_was_held = true;
 
-    #if defined (NXDK)
-    XVideoSetMode(640, 480, 32, REFRESH_DEFAULT);
-    debugClearScreen();
-    #endif
+  #if defined (NXDK)
+  XVideoSetMode(640, 480, 32, REFRESH_DEFAULT);
+  debugClearScreen();
+  #endif
 
-    SDL_Init(SDL_INIT_AUDIO|SDL_INIT_JOYSTICK);
+  SDL_Init(SDL_INIT_AUDIO|SDL_INIT_JOYSTICK);
 
-    // declare the needed variables
-    char* fileToPlay = "pausetest.wav";
-    SDL_AudioSpec wavSpec;
-    Uint32 wavLength;
-    Uint8 *wavBuffer;
-    SDL_GameController *controller;
-    int controllerport = 0;
-    char* controllername = NULL;
+  // declare the needed variables
+  char* fileToPlay = "pausetest.wav";
+  SDL_AudioSpec wavSpec;
+  Uint32 wavLength;
+  Uint8 *wavBuffer;
+  SDL_GameController *controller;
+  int controllerport = 0;
+  char* controllername = NULL;
 
-    for (int i = 0; i < SDL_NumJoysticks(); i++) { // For the time that i is smaller than the number of connected Joysticks
+  for (int i = 0; i < SDL_NumJoysticks(); i++) { // For the time that i is smaller than the number of connected Joysticks
 
-        if(SDL_IsGameController(i)) { // If i (which we use to iterate through the connected controllers) as a port number is a Game Controller
-            controller = SDL_GameControllerOpen(i); // Open the controller
+    if(SDL_IsGameController(i)) { // If i (which we use to iterate through the connected controllers) as a port number is a Game Controller
+      controller = SDL_GameControllerOpen(i); // Open the controller
 
-            if(controller) { // If we find that we opened a controller
-                controllerport = i;
-                controllername = SDL_GameControllerName(controller);
-                break; // Exit the loop
-            }
+      if(controller) { // If we find that we opened a controller
+        controllerport = i;
+        controllername = SDL_GameControllerName(controller);
+        break; // Exit the loop
+      }
                 
-        }
     }
+  }
     
-    SDL_LoadWAV(fileToPlay, &wavSpec, &wavBuffer, &wavLength);//FIXME: Ask for file at runtime
+  SDL_LoadWAV(fileToPlay, &wavSpec, &wavBuffer, &wavLength);//FIXME: Ask for file at runtime
 
-    SDL_AudioDeviceID deviceID = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0); //NULL means default
+  wavSpec.callback = audio_callback;
+	wavSpec.userdata = NULL;
 
-    int play = SDL_QueueAudio(deviceID, wavBuffer, wavLength); //Might compare to something for a feature in the future
+  audio_position = wavBuffer;
+	audio_length = wavLength;
 
-    #if !defined (NXDK)
+  SDL_AudioDeviceID deviceID = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0); //NULL means default
+
+  int play = SDL_QueueAudio(deviceID, wavBuffer, wavLength); //Might compare to something for a feature in the future
+
+  #if !defined (NXDK)
+  printf("Opened controller: %s on port %d\n", controllername, controllerport);
+  printf("Now playing: %s\n", fileToPlay);
+  #endif
+
+  while (audio_length > 0) {   
+    #if defined (NXDK)
+    XVideoWaitForVBlank();
+
+    debugClearScreen();
+
     printf("Opened controller: %s on port %d\n", controllername, controllerport);
     printf("Now playing: %s\n", fileToPlay);
     #endif
 
-    while (1) {   
-        #if defined (NXDK)
-        XVideoWaitForVBlank();
+      SDL_Event event;
 
-        debugClearScreen();
-
-        printf("Opened controller: %s on port %d\n", controllername, controllerport);
-        printf("Now playing: %s\n", fileToPlay);
-        #endif
-
-        SDL_Event event;
-
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT)
-            {
-                Quit(deviceID, wavBuffer);
-            }
+      while (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT)
+        {
+          Quit(deviceID, wavBuffer);
         }
+      }
         
+      if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_A)) {
+        SDL_PauseAudioDevice(deviceID, 1);
+      }
+      else {
+        SDL_PauseAudioDevice(deviceID, 0); //SDL_PauseAudioDevice with 0 in place of pause_on means unpaused, thus playing
+      }
+      //FIXME: Make it run for duration of song
+  }
 
-        if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_A)) {
-            SDL_PauseAudioDevice(deviceID, 1);
-        }
-        else {
-            SDL_PauseAudioDevice(deviceID, 0); //SDL_PauseAudioDevice with 0 in place of pause_on means unpaused, thus playing
-        }
-        //FIXME: Make it run for duration of song
-    }
-
-    return 0;
+  return 0;
 }
