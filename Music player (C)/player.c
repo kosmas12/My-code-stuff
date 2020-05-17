d17e17fc6f72bf7a146d6fdeaeb3e44d95d4c15d
Kosmas Raptis <keeperkosmas6@gmail.com>
2020-05-17 22:35:54 +0300

    There is a function to handle playing a file now, prepared to start work on file selector

--- Music player (C)/player.c
+++ Music player (C)/player.c
@@ -5,6 +5,7 @@
 #include <hal/debug.h>
 #include <SDL.h>
 #include <SDL_audio.h>
+#include <nxdk/mount.h>
 #define printf(...) debugPrint(__VA_ARGS__)
 #else
 #include <SDL2/SDL.h>
@@ -15,6 +16,9 @@
 
 static Uint8 *audio_position; 
 static Uint32 audio_length;
+Uint8 *wavBuffer;
+char* fileToPlay = "CivilSin.wav";
+SDL_AudioDeviceID deviceID;
 
 void audio_callback(void *userdata, Uint8 *stream, int len) {
 	
@@ -28,35 +32,28 @@ void audio_callback(void *userdata, Uint8 *stream, int len) {
 	audio_length -= len;
 }
 
-void Quit(SDL_AudioDeviceID deviceID, Uint8 *wavBuffer) {
-    SDL_CloseAudioDevice(deviceID);
+static void Quit(SDL_AudioDeviceID deviceID, Uint8 *wavBuffer) {
+  SDL_CloseAudioDevice(deviceID);
 	SDL_FreeWAV(wavBuffer);
 	SDL_Quit();
+} 
+
+static void PlayFile() {
+
+  Uint32 wavLength;
+  SDL_AudioSpec wavSpec;
+
+  SDL_LoadWAV(fileToPlay, &wavSpec, &wavBuffer, &wavLength);//FIXME: Ask for file at runtime
+  wavSpec.callback = audio_callback;
+	wavSpec.userdata = NULL;
+
+  audio_position = wavBuffer;
+	audio_length = wavLength;
+
+  deviceID = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0); //NULL means default
 }
 
-static bool isNewlyPressed(bool is_held, bool *was_held) {
-
-  if (is_held) { // If the button is held in this frame
-    if (*was_held) { // If it was held in the previous frame
-      return false; // The button isn't newly pressed
-    } 
-    else { // If it wasn't held in the previous frame
-      *was_held = true; // Set was_held to true because the current frame will be the previous frame in the next frame
-      return true; // The button is newly pressed, so we return true
-    } 
-        
-  } 
-  else { // If it isn't held in the current frame
-    *was_held = false; // Set was_held to false
-    return true; // The button isn't pressed at all, so we return false
-  }
-}
-
-int main()
-{
-
-  bool a_is_held = true;
-  bool a_was_held = true;
+static void Init() {
 
   #if defined (NXDK)
   XVideoSetMode(640, 480, 32, REFRESH_DEFAULT);
@@ -64,12 +61,10 @@ int main()
   #endif
 
   SDL_Init(SDL_INIT_AUDIO|SDL_INIT_JOYSTICK);
+}
 
-  // declare the needed variables
-  char* fileToPlay = "D:\\Pause Test.wav";
-  SDL_AudioSpec wavSpec;
-  Uint32 wavLength;
-  Uint8 *wavBuffer;
+int main()
+{
   SDL_GameController *controller;
   int controllerport = 0;
   char* controllername = NULL;
@@ -87,16 +82,8 @@ int main()
                 
     }
   }
-    
-  SDL_LoadWAV(fileToPlay, &wavSpec, &wavBuffer, &wavLength);//FIXME: Ask for file at runtime
 
-  wavSpec.callback = audio_callback;
-	wavSpec.userdata = NULL;
-
-  audio_position = wavBuffer;
-	audio_length = wavLength;
-
-  SDL_AudioDeviceID deviceID = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0); //NULL means default
+  PlayFile();
 
   #if !defined (NXDK)
   printf("Opened controller: %s on port %d\n", controllername, controllerport);
@@ -113,21 +100,21 @@ int main()
     printf("Now playing: %s\n", fileToPlay);
     #endif
 
-      SDL_Event event;
+    SDL_Event event;
 
-      while (SDL_PollEvent(&event)) {
-        if (event.type == SDL_QUIT)
-        {
-          Quit(deviceID, wavBuffer);
-        }
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT)
+      {
+        Quit(deviceID, wavBuffer);
       }
+    }
         
-      if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_A)) {
-        SDL_PauseAudioDevice(deviceID, 1);
-      }
-      else {
-        SDL_PauseAudioDevice(deviceID, 0); //SDL_PauseAudioDevice with 0 in place of pause_on means unpaused, thus playing
-      }
+    if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_A)) {
+      SDL_PauseAudioDevice(deviceID, 1);
+    }
+    else {
+      SDL_PauseAudioDevice(deviceID, 0); //SDL_PauseAudioDevice with 0 in place of pause_on means unpaused, thus playing
+    }
       
   }
 
