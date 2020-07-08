c9ba506f705023ac1448c17777bde53be0cff9a1
Kosmas Raptis <keeperkosmas6@gmail.com>
2020-07-09 00:05:46 +0300

    Code improvements

--- Music player (C)/player.c
+++ Music player (C)/player.c
@@ -44,54 +44,11 @@ static void Quit(Mix_Music *music, int exitcode) {
   Mix_FreeMusic(music);
   Mix_CloseAudio();
   Mix_Quit();
+  free(controller);
 	//SDL_FreeWAV(wavBuffer);
 	SDL_Quit();
   exit(exitcode);
 } 
-
-static bool isNewlyPressed(bool is_held, bool *was_held) {
-
-    if (is_held) { // If the button is held in this frame
-        if (*was_held) { // If it was held in the previous frame
-            return false; // The button isn't newly pressed
-        } 
-        else { // If it wasn't held in the previous frame
-            *was_held = true; // Set was_held to true because the current frame will be the previous frame in the next frame
-            return true; // The button is newly pressed, so we return true
-        } 
-        
-    } 
-    else { // If it isn't held in the current frame
-        *was_held = false; // Set was_held to false
-        return false; // The button isn't pressed at all, so we return false
-    }
-}
-
-float remap(float value, float from_min, float from_max, float to_min, float to_max) { // Function to remap values to other values
-  value = ((value - from_min) / (from_max - from_min)) * (to_max - to_min) + to_min;
-  return value;
-}
-
-static float getAxis(int sdl_axis) { // Function to get an axis from the controller
-
-  const float deadzone = 0.2f;
-
-  // Get input in range -1 to +1
-  float amount = (float)SDL_GameControllerGetAxis(controller, sdl_axis) / (float)0x8000;
-
-  // Reject if the stick is in deadzone
-  if (fabsf(amount) < deadzone) {
-    return 0.0f;
-  }
-
-  if (amount > 0.0f) {
-  amount = remap(amount,  deadzone, +1.0f,  0.0f, +1.0f); // Remap from +[0.2, 1.0] to +[0.0, 1.0]
-  } 
-  else {
-  amount = -remap(-amount,  deadzone, +1.0f,  0.0f, +1.0f); // Remap from -[0.2, 1.0] to -[0.0, 1.0]
-  }
-  return amount;
-}
 
 static void PlayFile() {
 
@@ -154,6 +111,7 @@ static void PlayFile() {
         printf("Couldn't open %s: %s\n",
         fileToPlay, Mix_GetError());
         Quit(music, 2);
+        free(rw);
     }
     printf("Loading %s\n", fileToPlay);
     music = Mix_LoadMUS_RW(rw, SDL_TRUE);
@@ -161,6 +119,7 @@ static void PlayFile() {
         printf("Couldn't load %s: %s\n",
         fileToPlay, Mix_GetError());
         Quit(music, 3);
+        free(rw);
     }
     printf("Loaded %s\n", fileToPlay);
     Mix_PlayMusic(music, looping);
@@ -178,7 +137,7 @@ static void Init() {
   SDL_Init(SDL_INIT_AUDIO|SDL_INIT_JOYSTICK);
 
   printf("Return value of Mix_Init(): %d\n", initted);
-  SDL_Delay(2000);
+  SDL_Delay(1500);
 
 
 }
@@ -260,7 +219,8 @@ static int FileBrowser() {
 
     printf("\nYour current selected file is: %s (Index number %d)\n", foundFiles[currentIndex].fileName, currentIndex);
     if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_A)) {
-      strcpy(fileToPlay, foundFiles[currentFileDirCount].filePath);
+      strcpy(fileToPlay, foundFiles[currentIndex].filePath);
+      free(foundFiles)
       break;
     }
   }
@@ -275,7 +235,7 @@ int main()
 
   //Open controller
   int controllerport = 0;
-  char* controllername = NULL;
+  const char* controllername = NULL;
 
   for (int i = 0; i < SDL_NumJoysticks(); i++) { // For the time that i is smaller than the number of connected Joysticks
 
@@ -303,7 +263,13 @@ int main()
   #else
   printf("Tell me the path to the file that you want to play: ");
   
-  scanf("%[^\t\n]", fileToPlay);
+  int numargs = scanf("%[^\t\n]", fileToPlay);
+  if (numargs == 0)
+  {
+    printf("No file entered. Exiting...");
+    Quit(music, 4);
+  }
+  
   #endif
 
   PlayFile();
@@ -311,6 +277,7 @@ int main()
   #if !defined (NXDK)
   printf("Opened controller: %s on port %d\n", controllername, controllerport);
   printf("Now playing: %s\n", fileToPlay);
+  free((char*)controllername);
   #endif
 
   while (Mix_PlayingMusic() == 1) {   
@@ -321,6 +288,8 @@ int main()
 
     printf("Opened controller: %s on port %d\n", controllername, controllerport);
     printf("Now playing: %s\n", fileToPlay);
+    free((char*)controllername);
+    free((int)controllerport);
     #endif
 
     SDL_Event event;
